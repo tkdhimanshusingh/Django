591994b85d1020a37b02ea5d17cfec23f9cfb215
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-06-21 10:36:39 +0530

    Home conflict resolved
    Initially the home.urls not contained contained path as "home", due to which it gave Error 404, now it have been resolved along with a extra comma removed from hello.urls

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Himanshu IceCream Admin"
+admin.site.site_title = "Himanshu IceCream Admin Portal"
+admin.site.index_title = "Welcome to Himanshu IceCream Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls')),
+    path('',include('home.urls'))
 ]
