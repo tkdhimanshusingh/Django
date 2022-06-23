4b84b1aef8d44e87f1919fff85f2a1aa8c05bed6
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-06-23 17:16:56 +0530

    Input data stored in data base.

--- hello/home/models.py
+++ hello/home/models.py
@@ -1,3 +1,10 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=122)
+    email=models.CharField(max_length=122)
+    phone=models.CharField(max_length=12)
+    desc=models.TextField()
+    date=models.DateField()
--- hello/home/views.py
+++ hello/home/views.py
@@ -1,5 +1,7 @@
+from asyncio import IocpProactor
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 def index(request):
     context={
@@ -14,5 +16,12 @@ def services(request):
     return render(request,'services.html')
     #return HttpResponse("This is Services page")
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
     return render(request,'contact.html')
     #return HttpResponse("This is Contact page")
